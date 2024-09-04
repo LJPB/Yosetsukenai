4cca865e0145e8d1cdc887c9cbe0c8cc1bc54eec
L.JP.B <dev@ljpb.me>
2024-09-04 20:17:03 +0900

    UUIDに関するTypeConverterの追加

--- app/src/main/java/me/ljpb/yosetsukenai/data/room/TableConverter.kt
+++ app/src/main/java/me/ljpb/yosetsukenai/data/room/TableConverter.kt
@@ -5,24 +5,35 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import me.ljpb.yosetsukenai.data.SimplePeriod
 import java.time.LocalDate
+import java.util.UUID
 
 class TableConverter {
+    // SimplePeriod
     @TypeConverter
     fun fromStringToSimplePeriod(string: String): SimplePeriod = SimplePeriod.fromString(string)
 
     @TypeConverter
     fun fromSimplePeriodToString(period: SimplePeriod): String = period.toString()
 
+    // LocalDate
     @TypeConverter
     fun fromStringToLocalDate(string: String): LocalDate = LocalDate.parse(string)
 
     @TypeConverter
     fun fromLocalDateToString(localDate: LocalDate): String = localDate.toString()
 
+    // List<String>
     @TypeConverter
     fun fromStringToStringList(string: String): List<String> =
         Json.decodeFromString<List<String>>(string)
 
     @TypeConverter
     fun fromStringListToString(stringList: List<String>): String = Json.encodeToString(stringList)
+
+    // UUID
+    @TypeConverter
+    fun fromStringToUuid(string: String): UUID = UUID.fromString(string)
+
+    @TypeConverter
+    fun fromUuidToString(uuid: UUID): String = uuid.toString()
 }
