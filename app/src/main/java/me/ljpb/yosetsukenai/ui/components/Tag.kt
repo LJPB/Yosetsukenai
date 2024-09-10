74b1bab2afe5d9010a6415da1005d975c8f0d2ed
L.JP.B <dev@ljpb.me>
2024-09-10 23:41:33 +0900

    場所や発見した虫を表示するタグの作成

---
+++ app/src/main/java/me/ljpb/yosetsukenai/ui/components/Tag.kt
@@ -0,0 +1,74 @@
+package me.ljpb.yosetsukenai.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+private fun Tag(
+    modifier: Modifier = Modifier,
+    color: Color,
+    text: String
+) {
+    Surface(
+        modifier = modifier,
+        color = color,
+        shape = MaterialTheme.shapes.small,
+    ) {
+        Text(
+            modifier = Modifier.padding(
+                vertical = 4.dp,
+                horizontal = 8.dp
+            ),
+            text = text,
+            style = MaterialTheme.typography.labelLarge
+        )
+    }
+}
+
+@Composable
+fun PlaceTag(
+    modifier: Modifier = Modifier, 
+    text: String
+) {
+    Tag(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.secondaryContainer,
+        text = text
+    )
+}
+
+@Composable
+fun InsectTag(
+    modifier: Modifier = Modifier, 
+    text: String
+) {
+    Tag(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.tertiaryContainer,
+        text = text
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PlaceTagPreview() {
+    PlaceTag(
+        modifier = Modifier.padding(8.dp),
+        text = "aaa"
+    )
+}
+@Preview(showBackground = true)
+@Composable
+private fun InsectTagPreview() {
+    InsectTag(
+        modifier = Modifier.padding(8.dp),
+        text = "aaa"
+    )
+}
