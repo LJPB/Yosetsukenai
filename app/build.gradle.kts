fab6ec7fb86548c7782be7db65c375678062c130
L.JP.B <dev@ljpb.me>
2024-09-11 16:53:38 +0900

    マテリアルアイコンの拡張

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation(libs.androidx.material.icons.extended)
+
     //Room
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
