0722f28e66b6e36fb477a5b5ecc29c593fa0385a
L.JP.B <dev@ljpb.me>
2024-09-13 05:27:42 +0900

    SimpleTextFieldのみを使うようにした

--- app/src/main/java/me/ljpb/yosetsukenai/ui/components/SimpleTextField.kt
+++ app/src/main/java/me/ljpb/yosetsukenai/ui/components/SimpleTextField.kt
@@ -1,11 +1,20 @@
 package me.ljpb.yosetsukenai.ui.components
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.OffsetMapping
@@ -13,6 +22,8 @@ import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import me.ljpb.yosetsukenai.R
 
 @Composable
 fun SimpleTextField(
@@ -80,6 +91,56 @@ fun SimpleTextField(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TextOnlyField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle,
+    textColor: Color,
+    placeholder: @Composable (() -> Unit)? = null,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    BasicTextField(
+        value = value,
+        modifier = modifier,
+        textStyle = textStyle.copy(color = textColor),
+        onValueChange = onValueChange,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        visualTransformation = visualTransformation,
+        singleLine = true,
+        decorationBox = @Composable { innerTextField ->
+            TextFieldDefaults.DecorationBox(
+                value = value,
+                visualTransformation = visualTransformation,
+                innerTextField = innerTextField,
+                placeholder = placeholder,
+                singleLine = true,
+                enabled = true,
+                interactionSource = remember { MutableInteractionSource() },
+                contentPadding = PaddingValues(0.dp),
+                colors = TextFieldDefaults.colors(
+                    // 背景を透明にする
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    errorContainerColor = Color.Transparent,
+
+                    // TextFieldの下線を透明にする
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    errorIndicatorColor = Color.Transparent
+                )
+            )
+        }
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 private fun SimpleTextFieldPreview() {
@@ -91,5 +152,22 @@ private fun SimpleTextFieldPreview() {
         textAlign = TextAlign.Center,
         placeholderText = "placeholder",
         placeholderTextColor = MaterialTheme.colorScheme.onSurfaceVariant
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TextOnlyFieldPreview() {
+    TextOnlyField(
+        modifier = Modifier.height(dimensionResource(id = R.dimen.row_item_height)),
+        value = "aaa",
+        onValueChange = {},
+        placeholder = {
+            Text(
+                text = "placeholder"
+            )
+        },
+        textColor = MaterialTheme.colorScheme.error,
+        textStyle = MaterialTheme.typography.bodyLarge.copy(textAlign = TextAlign.End)
     )
 }
