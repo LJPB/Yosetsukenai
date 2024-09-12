3f51f440ed2952018244e430e85343a939080829
L.JP.B <dev@ljpb.me>
2024-09-13 00:47:18 +0900

    アプリで共通して使うアイコンの定義

---
+++ app/src/main/java/me/ljpb/yosetsukenai/ui/components/Const.kt
@@ -0,0 +1,19 @@
+package me.ljpb.yosetsukenai.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.BugReport
+import androidx.compose.material.icons.outlined.CalendarMonth
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.PinDrop
+import androidx.compose.material.icons.outlined.Schedule
+import androidx.compose.material.icons.outlined.Science
+
+object ConstIcon {
+    val PRODUCT_NAME = Icons.Outlined.Science
+    val START_DATE = Icons.Outlined.CalendarMonth
+    val END_DATE = Icons.Outlined.CalendarMonth
+    val VALIDITY_PERIOD = Icons.Outlined.Schedule
+    val INSECT = Icons.Outlined.BugReport
+    val PLACE = Icons.Outlined.PinDrop
+    val NOTIFY = Icons.Outlined.Notifications
+}
