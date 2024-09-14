4305c9cfd21f7a844e71ef8cd0e5b28b4dbef9a7
L.JP.B <dev@ljpb.me>
2024-09-14 21:51:07 +0900

    rememberSaveableに変更

--- app/src/main/java/me/ljpb/yosetsukenai/ui/components/TextInputDialog.kt
+++ app/src/main/java/me/ljpb/yosetsukenai/ui/components/TextInputDialog.kt
@@ -8,7 +8,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -27,7 +27,7 @@ fun TextInputDialog(
     onSave: (String) -> Unit = {},
     onDismiss: () -> Unit = {}
 ) {
-    var text by remember { mutableStateOf(defaultValue) }
+    var text by rememberSaveable { mutableStateOf(defaultValue) }
     AlertDialog(
         modifier = modifier,
         onDismissRequest = onDismiss,
