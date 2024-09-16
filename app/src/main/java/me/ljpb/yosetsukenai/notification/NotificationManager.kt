a43d1202b6a5aecbf5c05891ccc21e12b5778c29
L.JP.B <dev@ljpb.me>
2024-09-17 03:37:42 +0900

    修正

--- app/src/main/java/me/ljpb/yosetsukenai/notification/NotificationManager.kt
+++ app/src/main/java/me/ljpb/yosetsukenai/notification/NotificationManager.kt
@@ -2,10 +2,9 @@ package me.ljpb.yosetsukenai.notification
 
 import me.ljpb.yosetsukenai.data.PeriodAndTime
 import me.ljpb.yosetsukenai.data.room.NotificationEntity
-import java.time.LocalDate
 
 interface NotificationManager {
-    fun createNotificationEntity(startDate: LocalDate, schedule: PeriodAndTime, parentId: Long): NotificationEntity
+    fun createNotificationEntity(triggerTimeSeconds: Long, schedule: PeriodAndTime, parentId: Long): NotificationEntity
 
     fun setNotification(notification: NotificationEntity)
 
