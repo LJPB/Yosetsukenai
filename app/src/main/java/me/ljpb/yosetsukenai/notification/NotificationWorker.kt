54e755e1c7bb382e1b31b70dd17556b352052af1
L.JP.B <dev@ljpb.me>
2024-09-17 03:32:40 +0900

    修正

--- app/src/main/java/me/ljpb/yosetsukenai/notification/NotificationWorker.kt
+++ app/src/main/java/me/ljpb/yosetsukenai/notification/NotificationWorker.kt
@@ -3,15 +3,9 @@ package me.ljpb.yosetsukenai.notification
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import me.ljpb.yosetsukenai.data.room.NotificationEntity
 
-class NotificationWorker(
-    private val notification: NotificationEntity,
-    appContext: Context,
-    params: WorkerParameters
-) : CoroutineWorker(appContext, params) {
+class NotificationWorker(appContext: Context, params: WorkerParameters) : CoroutineWorker(appContext, params) {
     override suspend fun doWork(): Result {
-        notify(applicationContext, notification)
         return Result.success()
     }
 }
