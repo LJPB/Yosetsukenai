2a1e1338884b580646af772ccab0c054b1ea7837
L.JP.B <dev@ljpb.me>
2024-09-17 03:46:30 +0900

    修正

--- app/src/main/java/me/ljpb/yosetsukenai/notification/NotificationManager.kt
+++ app/src/main/java/me/ljpb/yosetsukenai/notification/NotificationManager.kt
@@ -2,9 +2,19 @@ package me.ljpb.yosetsukenai.notification
 
 import me.ljpb.yosetsukenai.data.PeriodAndTime
 import me.ljpb.yosetsukenai.data.room.NotificationEntity
+import java.time.LocalDate
 
 interface NotificationManager {
-    fun createNotificationEntity(triggerTimeSeconds: Long, schedule: PeriodAndTime, parentId: Long): NotificationEntity
+    /**
+     * 日付 dateから期間 before前に通知するようなEntityを取得する
+     * たとえば，date = 2024/10/01, before = 1日前 なら 通知する日にちは 2024/10/01 - 1日 = 2024/09/30
+     * もちろん，before = 0 なら通知する日にちはdate
+     */
+    fun createNotificationEntity(
+        date: LocalDate,
+        before: PeriodAndTime,
+        parentId: Long
+    ): NotificationEntity
 
     fun setNotification(notification: NotificationEntity)
 
