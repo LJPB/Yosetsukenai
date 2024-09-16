317a0a784cd6c809be5123c4e225459ed38242be
L.JP.B <dev@ljpb.me>
2024-09-17 02:44:05 +0900

    型の間違いの修正

--- app/src/main/java/me/ljpb/yosetsukenai/notification/NotificationManager.kt
+++ app/src/main/java/me/ljpb/yosetsukenai/notification/NotificationManager.kt
@@ -1,13 +1,12 @@
 package me.ljpb.yosetsukenai.notification
 
-import android.app.Notification
 import me.ljpb.yosetsukenai.data.PeriodAndTime
 import me.ljpb.yosetsukenai.data.room.NotificationEntity
 
 interface NotificationManager {
     fun createNotificationEntity(periodAndTime: PeriodAndTime, parentId: Long): NotificationEntity
     
-    fun setNotification(notification: Notification)
+    fun setNotification(notification: NotificationEntity)
     
-    fun cancelNotify(notification: Notification)
+    fun cancelNotify(notification: NotificationEntity)
 }
