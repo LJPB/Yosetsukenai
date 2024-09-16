a749d7005dbb9d0a369745d319fa10049976d97b
L.JP.B <dev@ljpb.me>
2024-09-17 04:05:17 +0900

    インターフェースではなく普通のクラス(object)に変更

--- app/src/main/java/me/ljpb/yosetsukenai/notification/NotificationManager.kt
+++ app/src/main/java/me/ljpb/yosetsukenai/notification/NotificationManager.kt
@@ -5,7 +5,7 @@ import me.ljpb.yosetsukenai.data.room.NotificationEntity
 import java.time.LocalDate
 import java.time.ZoneId
 
-interface NotificationManager {
+object NotificationManager {
     /**
      * 日付 dateから期間 before前に通知するようなEntityを取得する
      * たとえば，date = 2024/10/01, before = 1日前 なら 通知する日にちは 2024/10/01 - 1日 = 2024/09/30
@@ -16,11 +16,19 @@ interface NotificationManager {
         before: PeriodAndTime,
         parentId: Long,
         zoneId: ZoneId
-    ): NotificationEntity
+    ): NotificationEntity {
+        TODO()
+    }
 
-    fun setNotification(notification: NotificationEntity)
+    fun setNotification(notification: NotificationEntity, title: String, text: String) {
+        
+    }
 
-    fun updateNotifySchedule(updatedNotificationEntity: NotificationEntity)
+    fun updateNotifySchedule(updatedNotificationEntity: NotificationEntity, title: String, text: String) {
+        
+    }
 
-    fun cancelNotify(notification: NotificationEntity)
+    fun cancelNotify(notification: NotificationEntity) {
+        
+    }
 }
