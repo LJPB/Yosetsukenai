e5b0f718255aa50fab0158b8d4c1a93666b6c479
LJPB <dev@ljpb.me>
2024-09-25 03:32:24 +0900

    虫除けの編集/追加画面の追加

---
+++ app/src/main/java/me/ljpb/yosetsukenai/ui/screens/edit/RepellentEditScreen.kt
@@ -0,0 +1,21 @@
+package me.ljpb.yosetsukenai.ui.screens.edit
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import me.ljpb.yosetsukenai.ui.components.edit.RepellentEditContent
+
+@Composable
+fun RepellentEditScreen(
+    modifier: Modifier = Modifier,
+    repellentEditViewModel: RepellentEditViewModel,
+    isLandscape: Boolean,
+    onCancel: () -> Unit,
+) {
+    // TODO: 諸々の処理 
+    RepellentEditContent(
+        modifier = modifier,
+        repellentEditViewModel = repellentEditViewModel,
+        isLandscape = isLandscape,
+        onCancel = onCancel
+    )
+}
