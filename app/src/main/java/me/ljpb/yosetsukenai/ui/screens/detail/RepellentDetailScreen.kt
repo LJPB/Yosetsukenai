c11a511d5e70ad009f25e91ea2f9e80abe3e9a3a
LJPB <dev@ljpb.me>
2024-09-30 05:00:29 +0900

    修正

--- app/src/main/java/me/ljpb/yosetsukenai/ui/screens/detail/RepellentDetailScreen.kt
+++ app/src/main/java/me/ljpb/yosetsukenai/ui/screens/detail/RepellentDetailScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import me.ljpb.yosetsukenai.data.room.InsectEntity
+import me.ljpb.yosetsukenai.data.room.NotificationEntity
 import me.ljpb.yosetsukenai.data.room.RepellentScheduleEntity
 import me.ljpb.yosetsukenai.ui.components.detail.DetailContainer
 import me.ljpb.yosetsukenai.ui.components.detail.RepellentDetailContent
@@ -15,14 +16,14 @@ fun RepellentDetailScreen(
     repellentDetailViewModel: RepellentDetailViewModel,
     insectOnClick: (InsectEntity) -> Unit,
     backButtonOnClick: () -> Unit,
-    editButtonOnClick: (RepellentScheduleEntity) -> Unit,
+    editButtonOnClick: (RepellentScheduleEntity, List<NotificationEntity>) -> Unit,
 ) {
     val insects by repellentDetailViewModel.insects.collectAsState()
     val notifications by repellentDetailViewModel.notifications.collectAsState()
     val repellent = repellentDetailViewModel.repellent
     DetailContainer(
         backButtonOnClick = backButtonOnClick,
-        editButtonOnClick = { editButtonOnClick(repellent) }
+        editButtonOnClick = { editButtonOnClick(repellent, notifications) }
     ) {
         RepellentDetailContent(
             modifier = modifier,
