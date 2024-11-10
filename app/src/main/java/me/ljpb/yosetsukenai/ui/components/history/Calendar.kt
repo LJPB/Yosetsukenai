8d342ee56bf8fc50a6cb9a9daf7757aa601df700
ljpb <dev@ljpb.me>
2024-11-10 22:43:12 +0900

    対応する日付に記録があれば表示するように修正

--- app/src/main/java/me/ljpb/yosetsukenai/ui/components/history/Calendar.kt
+++ app/src/main/java/me/ljpb/yosetsukenai/ui/components/history/Calendar.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,15 +18,22 @@ import androidx.compose.ui.unit.dp
 import com.kizitonwose.calendar.compose.CalendarState
 import com.kizitonwose.calendar.compose.VerticalCalendar
 import com.kizitonwose.calendar.compose.rememberCalendarState
+import com.kizitonwose.calendar.core.CalendarDay
 import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import me.ljpb.yosetsukenai.ui.screens.history.HistoryScreenViewModel
+import java.time.LocalDate
 import java.time.YearMonth
 
 @Composable
 fun Calendar(
     modifier: Modifier = Modifier,
-    calendarState: CalendarState
+    calendarState: CalendarState,
+    viewModel: HistoryScreenViewModel,
+    onDayClick: (CalendarDay) -> Unit,
 ) {
     val dayTextStyle = MaterialTheme.typography.bodyMedium
+    val days by viewModel.days.collectAsState()
+    val dayStates = hashMapOf<LocalDate, State<Boolean>>()
     Box(
         modifier = modifier,
         contentAlignment = Alignment.Center
@@ -39,10 +49,16 @@ fun Calendar(
                 )
             },
             dayContent = {
+                viewModel.load(it.date)
+                dayStates[it.date] = days[it.date]!!.collectAsState()
+                val state by dayStates[it.date]!!
                 Day(
+                    modifier = Modifier.padding(4.dp),
                     day = it,
-                    textStyle = dayTextStyle
-                ) { }
+                    textStyle = dayTextStyle,
+                    clickState = state,
+                    onClick = onDayClick
+                )
             },
             monthContainer = { _, container ->
                 Column(
@@ -70,8 +86,8 @@ private fun CalendarPreview() {
         firstVisibleMonth = currentMonth,
         firstDayOfWeek = firstDayOfWeek
     )
-    Calendar(
-        modifier = Modifier,
-        calendarState = state
-    )
+//    Calendar(
+//        modifier = Modifier,
+//        calendarState = state
+//    )
 }
